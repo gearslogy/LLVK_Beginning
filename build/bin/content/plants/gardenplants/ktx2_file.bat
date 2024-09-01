16e2e75f82c73f671e9281ed117a2879df8d8287
Gearslogy <liuyangping207@qq.com>
2024-09-01 14:19:47 +0800

    shadow map resources

---
+++ build/bin/content/plants/gardenplants/ktx2_file.bat
@@ -0,0 +1,8 @@
+ktx create --format=R8G8B8_UNORM --generate-mipmap --encode=uastc --layers=6 ^
+ForestFern_2K_Albedo.jpg ^
+ForestFern_2K_Normal.jpg ^
+ForestFern_2K_Roughness.jpg ^
+ForestFern_2K_Displacement.jpg ^
+ForestFern_2K_Opacity.jpg ^
+ForestFern_2K_Translucency.jpg ^
+tex_array.ktx2
---
+++ build/bin/content/shadowmap/grid_tex/ktx2_file.bat
@@ -0,0 +1,7 @@
+ktx create --format=R8G8B8_UNORM --generate-mipmap --encode=uastc --layers=5 ^
+vddidey_4K_Albedo.jpg ^
+vddidey_4K_Normal.jpg ^
+vddidey_4K_Roughness.jpg ^
+vddidey_4K_Displacement.jpg ^
+vddidey_4K_AO.jpg ^
+tex_array.ktx2
