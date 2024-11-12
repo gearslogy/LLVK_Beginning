54eb002687894d386f8a6deeef905d456c21edd0
Gearslogy <liuyangping207@qq.com>
2024-11-12 13:37:06 +0800

    Render delegate

---
+++ renderer/public/GeometryContainers.cpp
@@ -0,0 +1,69 @@
+//
+// Created by liuya on 11/11/2024.
+//
+
+#include "GeometryContainers.h"
+#include "../../LLVK_Descriptor.hpp"
+#include "../../VulkanRenderer.h"
+LLVK_NAMESPACE_BEGIN
+
+template<typename T>
+inline void updateWriteSets(const auto &requiredObjects , const std::vector<T> &renderDelegates, uint32_t texBindingIdOffset) {
+    const auto &device = requiredObjects.pVulkanRenderer->getMainDevice().logicalDevice;
+    for(int frame=0;frame<MAX_FRAMES_IN_FLIGHT;frame++) {
+        const auto &uboBuffer = *requiredObjects.pUBOs[frame];
+        for(const auto &geo: renderDelegates) {
+            const auto &ubo_set = geo.setUBOs[frame];
+            const auto &tex_set = geo.setTextures[frame];
+            // UBO
+            std::vector<VkWriteDescriptorSet> writeSets;
+            writeSets.emplace_back(FnDescriptor::writeDescriptorSet(ubo_set,VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,0, &uboBuffer.descBufferInfo));
+            // tex
+            for(auto &&[idx, tex] : UT_Fn::enumerate(geo.pTextures))
+                writeSets.emplace_back(FnDescriptor::writeDescriptorSet(tex_set,VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,idx + texBindingIdOffset, &tex->descImageInfo));
+            vkUpdateDescriptorSets(device,static_cast<uint32_t>(writeSets.size()),writeSets.data(),0, nullptr);
+        }
+    }
+}
+
+
+void RenderContainerTwoSet::buildSet() {
+    const auto &device = requiredObjects.pVulkanRenderer->getMainDevice().logicalDevice;
+    const auto &pool = *requiredObjects.pPool;
+    const auto &ubo_setLayout = *requiredObjects.pSetLayoutUBO;
+    const auto &tex_setLayout = *requiredObjects.pSetLayoutTexture;
+
+    const std::vector ubo_layouts(MAX_FRAMES_IN_FLIGHT, ubo_setLayout);
+    const std::vector tex_layouts(MAX_FRAMES_IN_FLIGHT, tex_setLayout);
+    auto uboSetAllocInfo = FnDescriptor::setAllocateInfo(pool,ubo_layouts);
+    auto texSetAllocInfo = FnDescriptor::setAllocateInfo(pool,tex_layouts);
+    auto allocateSetForObjects = [&](std::vector<RenderDelegate> &objs) {
+        for(auto &geo : objs) {
+            UT_Fn::invoke_and_check("Error create RenderContainerTwoSet::uboSets",vkAllocateDescriptorSets,device, &uboSetAllocInfo,geo.setUBOs.data());
+            UT_Fn::invoke_and_check("Error create RenderContainerTwoSet::texSets",vkAllocateDescriptorSets,device, &texSetAllocInfo,geo.setTextures.data());
+        }
+    };
+    allocateSetForObjects(renderDelegates);
+    updateWriteSets(requiredObjects, renderDelegates, 0U);
+
+}
+
+
+void RenderContainerOneSet::buildSet() {
+    const auto &device = requiredObjects.pVulkanRenderer->getMainDevice().logicalDevice;
+    const auto &pool = *requiredObjects.pPool;
+    const auto &setLayout = *requiredObjects.pSetLayout;
+    const std::vector setLayouts(MAX_FRAMES_IN_FLIGHT, setLayout);
+    auto setAllocInfo = FnDescriptor::setAllocateInfo(pool,setLayouts);
+    for(auto &geo : renderDelegates) {
+        UT_Fn::invoke_and_check("Error create RenderContainerOneSet::uboSets", vkAllocateDescriptorSets, device, &setAllocInfo, geo.setUBOs.data());
+        UT_Fn::invoke_and_check("Error create RenderContainerOneSet::texSets", vkAllocateDescriptorSets, device, &setAllocInfo, geo.setTextures.data());
+    }
+    updateWriteSets(requiredObjects, renderDelegates, 1U);
+}
+
+
+
+
+
+LLVK_NAMESPACE_END
---
+++ renderer/public/GeometryContainers.h
@@ -0,0 +1,113 @@
+//
+// Created by liuya on 11/11/2024.
+//
+
+#ifndef CSM_GEOMETRYCONTAINERS_H
+#define CSM_GEOMETRYCONTAINERS_H
+
+
+#include "../../LLVK_GeomtryLoader.h"
+#include "../../LLVK_VmaBuffer.h"
+
+
+LLVK_NAMESPACE_BEGIN
+
+class VulkanRenderer;
+/* at future !
+enum class SetNumType {
+    UBO_NONE_TEXTURE_NONE,
+    SET0_UBO_BINDING_0_N,
+    SET0_UBO_BINDING_0_TEXTURE_BINDING_1_N,
+    SET0_UBO_BINDING_0_AND_SET1_TEXTURE_BINDING_0_N
+};
+template<SetNumType SetNumType>
+struct RenderDelegate;
+
+template<>
+struct RenderDelegate<SetNumType::SET0_UBO_BINDING_0_N> {
+    const GLTFLoader::Part *pGeometry;
+    std::array<VkDescriptorSet,MAX_FRAMES_IN_FLIGHT> setUBOs;
+};
+
+template<>
+struct RenderDelegate<SetNumType::SET0_UBO_BINDING_0_TEXTURE_BINDING_1_N> {
+    const GLTFLoader::Part *pGeometry;
+    std::vector<const IVmaUBOTexture *> pTextures; // use this to support multi textures
+    std::array<VkDescriptorSet,MAX_FRAMES_IN_FLIGHT* 2 > sets; // ubo tex | ubo tex |
+    void bindTextures(auto && ... textures) {(pTextures.emplace_back(textures), ... );}
+};
+
+// Required object interface
+template<SetNumType SetNumType>
+struct RequiredObjects;
+
+template<>
+struct RequiredObjects<SetNumType::SET0_UBO_BINDING_0_N> {
+    const VulkanRenderer *pVulkanRenderer;
+    const VkDescriptorPool *pPool;                     // ref:pool allocate sets
+    std::array<const VmaUBOBuffer *,MAX_FRAMES_IN_FLIGHT >pUBOs;          // MAX FLIGHT
+    const VkDescriptorSetLayout *pSetLayoutUBO;        // set=0
+};
+
+template<>
+struct RequiredObjects<SetNumType::SET0_UBO_BINDING_0_TEXTURE_BINDING_1_N> {
+    const VulkanRenderer *pVulkanRenderer;
+    const VkDescriptorPool *pPool;                     // ref:pool allocate sets
+    std::array<const VmaUBOBuffer *,MAX_FRAMES_IN_FLIGHT >pUBOs;          // MAX FLIGHT
+    const VkDescriptorSetLayout *pSetLayout;        // set=0
+};
+*/
+
+
+
+struct RenderContainerTwoSet {
+    struct RenderDelegate {
+        const GLTFLoader::Part *pGeometry;
+        std::vector<const IVmaUBOTexture *> pTextures; // use this to support multi textures
+        std::array<VkDescriptorSet,MAX_FRAMES_IN_FLIGHT> setUBOs;       // set=0
+        std::array<VkDescriptorSet,MAX_FRAMES_IN_FLIGHT> setTextures;   // set=1
+        void bindTextures(auto && ... textures) {(pTextures.emplace_back(textures), ... );}
+    };
+
+    struct RequiredObjects{
+        const VulkanRenderer *pVulkanRenderer;
+        const VkDescriptorPool *pPool;                     // ref:pool allocate sets
+        std::array<const VmaUBOBuffer *,MAX_FRAMES_IN_FLIGHT >pUBOs;          // MAX FLIGHT
+        const VkDescriptorSetLayout *pSetLayoutUBO;        // set=0
+        const VkDescriptorSetLayout *pSetLayoutTexture;    // set=1
+    };
+    void setRequiredObjects( RequiredObjects &&rRequiredObjects) { requiredObjects = rRequiredObjects;}
+    void buildSet();
+private:
+    RequiredObjects requiredObjects{};
+    std::vector<RenderDelegate> renderDelegates{};
+};
+
+struct RenderContainerOneSet {
+    struct RenderDelegate {
+        const GLTFLoader::Part *pGeometry;
+        std::vector<const IVmaUBOTexture *> pTextures; // use this to support multi textures
+        std::array<VkDescriptorSet, MAX_FRAMES_IN_FLIGHT> setUBOs;       // shared set=0
+        std::array<VkDescriptorSet, MAX_FRAMES_IN_FLIGHT> setTextures;   // shared set=0
+        void bindTextures(auto && ... textures) {(pTextures.emplace_back(textures), ... );}
+    };
+    struct RequiredObjects{
+        const VulkanRenderer *pVulkanRenderer;
+        const VkDescriptorPool *pPool;                     // ref:pool allocate sets
+        std::array<const VmaUBOBuffer *,MAX_FRAMES_IN_FLIGHT >pUBOs;          // MAX FLIGHT
+        const VkDescriptorSetLayout *pSetLayout;        // set=0
+    };
+    void setRequiredObjects( RequiredObjects &&rRequiredObjects) { requiredObjects = rRequiredObjects;}
+    void buildSet();
+private:
+    RequiredObjects requiredObjects{};
+    std::vector<RenderDelegate> renderDelegates{};
+};
+
+
+
+
+
+LLVK_NAMESPACE_END
+
+#endif //CSM_GEOMETRYCONTAINERS_H
