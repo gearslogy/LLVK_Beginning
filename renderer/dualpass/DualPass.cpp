08844fa5c82db45f822dc93e03b0125008dca585
Gearslogy <liuyangping207@qq.com>
2024-11-24 02:16:00 +0800

    renderpass and pipeline for render opaque geometry

--- renderer/dualpass/DualPass.cpp
+++ renderer/dualpass/DualPass.cpp
@@ -1,10 +1,122 @@
-//
+﻿//
 // Created by liuya on 11/11/2024.
 //
 
 #include "DualPass.h"
-
 #include <LLVK_SYS.hpp>
+#include "LLVK_Descriptor.hpp"
+#include "LLVK_RenderPass.hpp"
+#include "UT_DualVkRenderPass.hpp"
+#include "DualPassGlobal.hpp"
+#include "VulkanRenderer.h"
+#include "DualPassRenderer.h"
+#include "renderer/public/GeometryContainers.h"
 LLVK_NAMESPACE_BEGIN
+OpaqueScenePass::OpaqueScenePass(DualPassRenderer *renderer): pRenderer(renderer){
+
+}
+
+void OpaqueScenePass::prepare() {
+    // desc set
+    createRenderPass();
+    createFramebuffer();
+    createPipeline();
+}
+
+void OpaqueScenePass::recordCommandBuffer() {
+    // UBO is shared, so we dont need ubo update here.
+    auto [width, height] = pRenderer->getSwapChainExtent();
+    VkDeviceSize offsets[1] = { 0 };
+    auto viewport = FnCommand::viewport(width,height );
+    auto scissor = FnCommand::scissor(width,height);
+    // clear
+    std::vector<VkClearValue> clearValues{};
+    clearValues.resize(2);
+    clearValues[0].color = { { 0.0f, 0.0f, 0.0f, 0.0f } };
+    clearValues[1].depthStencil = { 1.0f, 0 };
+
+    const auto renderPassBeginInfo= FnCommand::renderPassBeginInfo(framebuffer, renderPass, pRenderer->getSwapChainExtent(),clearValues);
+    const auto cmdBuf = pRenderer->getMainCommandBuffer();
+    vkCmdBeginRenderPass(cmdBuf, &renderPassBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBindPipeline(cmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS , pipeline);
+    vkCmdBindDescriptorSets(cmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS, pRenderer->dualPipelineLayout, 0, 1, &sets[pRenderer->getCurrentFrame()], 0, nullptr);
+    UT_GeometryContainer::renderPart(cmdBuf, &pRenderer->gridLoader.parts[0]);
+    vkCmdSetViewport(cmdBuf, 0, 1, &viewport);
+    vkCmdSetScissor(cmdBuf,0, 1, &scissor);
+    vkCmdEndRenderPass(cmdBuf);
+
+}
+
+void OpaqueScenePass::cleanup() {
+    const auto device = pRenderer->getMainDevice().logicalDevice;
+    UT_Fn::cleanup_render_pass(device, renderPass);
+    UT_Fn::cleanup_pipeline(device, pipeline);
+    UT_Fn::cleanup_framebuffer(device, framebuffer);
+}
+
+
+void OpaqueScenePass::onSwapChainResize() {
+    const auto &device = pRenderer->getMainDevice().logicalDevice;
+    UT_Fn::cleanup_framebuffer(device, framebuffer);
+    createFramebuffer(); // rebuild framebuffer
+}
+
+void OpaqueScenePass::createFramebuffer() {
+    const auto device = pRenderer->getMainDevice().logicalDevice;
+    const auto [width,height ] = pRenderer->getSwapChainExtent();
+    std::array attachments = {
+        pRenderer->renderTargets.colorAttachment.view,
+        pRenderer->renderTargets.depthAttachment.view
+    };
+    auto fbCIO = FnRenderPass::framebufferCreateInfo(width,height, renderPass,attachments);
+    if (vkCreateFramebuffer(device, &fbCIO, nullptr, &framebuffer) != VK_SUCCESS)
+        throw std::runtime_error("failed to create main pass framebuffer!");
+}
+
+
+void OpaqueScenePass::createRenderPass() {
+    const auto cdATM = FnRenderPass::colorAttachmentDescription(DualPassGlobal::colorAttachmentFormat);
+    const auto depthATM = FnRenderPass::depthAttachmentDescription(DualPassGlobal::depthStencilAttachmentFormat);
+    const auto cdATMRef = FnRenderPass::attachmentReference(0,VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    const auto depthATMRef = FnRenderPass::attachmentReference(1,VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL);
+    const auto subpass = FnRenderPass::subpassDescription(cdATMRef,  depthATMRef);
+
+    VkSubpassDependency dependency{};
+    dependency.srcSubpass = VK_SUBPASS_EXTERNAL;  // before renderpass
+    dependency.dstSubpass = 0;
+    dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;    // before stage
+    dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;   // after stage
+    dependency.srcAccessMask = 0; // before access 首次写入，不需要等待之前的访问
+    dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT ; // after access
+
+    std::array attachments = {cdATM, depthATM};
+    auto renderPassCIO = FnRenderPass::renderPassCreateInfo(attachments);
+    renderPassCIO.subpassCount = 1;
+    renderPassCIO.pSubpasses = &subpass;
+    renderPassCIO.dependencyCount = 1;
+    renderPassCIO.pDependencies = &dependency;
+    // create
+    const auto device = pRenderer->getMainDevice().logicalDevice;
+    const auto ret = vkCreateRenderPass(device, &renderPassCIO, nullptr, &renderPass);
+    if(ret != VK_SUCCESS) throw std::runtime_error{"ERROR"};
+}
+void OpaqueScenePass::createPipeline() {
+    const auto device = pRenderer->getMainDevice().logicalDevice;
+    const auto vsMD = FnPipeline::createShaderModuleFromSpvFile("shaders/indirect_instance_vert.spv",  device);    //shader modules
+    const auto fsMD = FnPipeline::createShaderModuleFromSpvFile("shaders/indirect_instance_frag.spv",  device);
+    VkPipelineShaderStageCreateInfo vsMD_ssCIO = FnPipeline::shaderStageCreateInfo(VK_SHADER_STAGE_VERTEX_BIT, vsMD);    //shader stages
+    VkPipelineShaderStageCreateInfo fsMD_ssCIO = FnPipeline::shaderStageCreateInfo(VK_SHADER_STAGE_FRAGMENT_BIT, fsMD);
+    pso.setShaderStages(vsMD_ssCIO, fsMD_ssCIO);
+    pso.setShaderModule(vsMD,fsMD);
+    pso.setPipelineLayout(pRenderer->dualPipelineLayout);
+    pso.setRenderPass(renderPass);
+    pso.rasterizerStateCIO.cullMode = VK_CULL_MODE_NONE;
+    UT_GraphicsPipelinePSOs::createPipeline(device, pso, pRenderer->getPipelineCache(), pipeline);
+    pso.cleanupShaderModule();
+}
+
+
+
+
 
 LLVK_NAMESPACE_END
--- renderer/dualpass/DualPass.h
+++ renderer/dualpass/DualPass.h
@@ -5,12 +5,37 @@
 #ifndef DUALPASS_H
 #define DUALPASS_H
 
+#include <LLVK_UT_Pipeline.hpp>
+#include <vulkan/vulkan.h>
+#include "LLVK_SYS.hpp"
+#include "LLVK_VmaBuffer.h"
 
+LLVK_NAMESPACE_BEGIN
 
-struct DualPass {
-
+struct DualPassRenderer;
+// our first render pass
+struct OpaqueScenePass {
+    OpaqueScenePass(DualPassRenderer *renderer);
+    void prepare();
+    void recordCommandBuffer();
+    void cleanup();
+    void onSwapChainResize();
+    // bind object
+private:
+    // created
+    void createRenderPass();
+    void createFramebuffer();
+    void createPipeline();
+    DualPassRenderer *pRenderer;
+    VkRenderPass renderPass{};
+    VkFramebuffer framebuffer{};
+    VkPipeline pipeline{};
+    UT_GraphicsPipelinePSOs pso{};
+    VkDescriptorSet sets[2]{};
 };
 
+
+LLVK_NAMESPACE_END
 
 
 #endif //DUALPASS_H
