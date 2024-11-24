a45f4f540b0376ef1016d8e78805fcfb115e36c9
Gearslogy <liuyangping207@qq.com>
2024-11-24 23:51:40 +0800

    add occ support

--- build/bin/content/scene/dualpass/generate_ktx2..bat
+++ build/bin/content/scene/dualpass/generate_ktx2..bat
@@ -2,5 +2,5 @@
 set TEX_OPTIONS=--format=R8G8B8A8_SRGB --generate-mipmap --encode=uastc
 
 ktx create %TEX_OPTIONS% head.png gpu_head_D.ktx2
-ktx create %TEX_OPTIONS% hair.png gpu_hair_D.ktx2
+ktx create %TEX_OPTIONS% hair_occ.png gpu_hair_D.ktx2
 ktx create %TEX_OPTIONS% grid.jpg gpu_grid_D.ktx2
