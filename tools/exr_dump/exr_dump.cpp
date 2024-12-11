2ded0603c3ad232d85b987c11a2704a09f7e7308
Gearslogy <liuyangping207@qq.com>
2024-12-12 00:36:00 +0800

    ready load exr

---
+++ tools/exr_dump/exr_dump.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liuya on 12/11/2024.
+//
+
+#include "exr_dump.h"
---
+++ tools/exr_dump/exr_dump.h
@@ -0,0 +1,16 @@
+//
+// Created by liuya on 12/11/2024.
+//
+
+#ifndef EXR_DUMP_H
+#define EXR_DUMP_H
+
+
+
+class exr_dump {
+
+};
+
+
+
+#endif //EXR_DUMP_H
