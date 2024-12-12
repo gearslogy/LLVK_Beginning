158e35ce5d69cbc583bc5b2bc909cb71a748d0ef
Gearslogy <liuyangping207@qq.com>
2024-12-13 02:39:25 +0800

    New VertexFormat

--- LLVK_GeomtryLoader.h
+++ LLVK_GeomtryLoader.h
@@ -33,12 +33,33 @@ struct GLTFVertex{
     static std::array<VkVertexInputAttributeDescription,7> attribs();
     // instance
 };
+
+struct GLTFVertexVATFracture {
+    glm::vec3 P{};      // 0
+    glm::vec3 Cd{};     // 1
+    glm::vec3 N{};      // 2
+    glm::vec3 T{};      // 3
+    glm::vec3 B{};      // 4
+    glm::vec2 uv0{};    // 5
+    glm::int32_t fractureIndex{};
+};
+inline constexpr auto GLTF_VertexHash = [](auto vertex) {
+    return ((std::hash<glm::vec3>()(vertex.P) ^ (std::hash<glm::vec3>()(vertex.Cd) << 1)) >> 1) ^ (std::hash<glm::vec2>()(vertex.uv0) << 1);
+};
+
 LLVK_NAMESPACE_END
 
 namespace std {
     template<> struct hash<LLVK::GLTFVertex> {
         size_t operator()(LLVK::GLTFVertex const& vertex) const {
-            return ((hash<glm::vec3>()(vertex.P) ^ (hash<glm::vec3>()(vertex.Cd) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.uv0) << 1);
+            //return ((hash<glm::vec3>()(vertex.P) ^ (hash<glm::vec3>()(vertex.Cd) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.uv0) << 1);
+            return LLVK::GLTF_VertexHash(vertex);
+        }
+    };
+    template<> struct hash<LLVK::GLTFVertexVATFracture> {
+        size_t operator()(LLVK::GLTFVertexVATFracture const& vertex) const {
+            //return ((hash<glm::vec3>()(vertex.P) ^ (hash<glm::vec3>()(vertex.Cd) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.uv0) << 1);
+            return LLVK::GLTF_VertexHash(vertex);
         }
     };
 }
